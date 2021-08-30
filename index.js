dd9292b29a50357fd5eb089f9fc935c4266389fc
Nyuta B <anyuta.nyuta333@gmail.com>
2021-08-30 12:53:29 +0300

    4th commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'data.log' }),
+    new winston.transports.File({ filename: 'data.log' })
   ]
 })
 
@@ -42,67 +42,10 @@ const linkM = 'https://www.saksfifthavenue.com/c/men/apparel';
 
       let counter = 0
 
-      // autoScroll page to the bottom to get all divs
-
-      async function autoScroll (page) {
-        await page.evaluate(async () => {
-          await new Promise((resolve) => {
-            let totalHeight = 0
-            const distance = 100
-            const timer = setInterval(() => {
-              const scrollHeight = document.body.scrollHeight
-              window.scrollBy(0, distance)
-              totalHeight += distance
-
-              if (totalHeight >= scrollHeight) {
-                clearInterval(timer)
-                resolve()
-              }
-            }, 80)
-          })
-        })
-      }
-
-      // function to parse longDescription
-
-      function parseLongDesc (text) {
-        const cutText = [] // new array without first sentence of description
-        let joinedText
-        const newText = []
-        let counter = 0
-
-        for (let i = 0; i < text.length; i++) {
-          if (text[i] === '<') {
-            counter = i
-          }
-          if (counter > 0) {
-            cutText[i] = text[i]
-          }
-        }
-
-        joinedText = cutText.join('')
-
-        const noSlashText = joinedText.replace(/[/]/g, '')
-        const noLiText = noSlashText.replace(/<li>/g, ' ')
-        const noUlText = noLiText.replace(/<ul>/g, '')
-
-        for (let i = 0; i < noUlText.length; i++) {
-          if (noUlText[i] !== '<') {
-            newText[i] = noUlText[i]
-          } else {
-            i = noUlText.length
-          }
-        }
-
-        const newLine = newText.join('')
-
-        return newLine.replace(/  +/g, ', ')
-      }
-
       logger.info('Page: ' + (counter + 1))
       logger.info('Scrolling page')
 
-      //await autoScroll(page)
+      // await autoScroll(page)
       async function autoScrollTo (page) {
         await page.evaluate(async () => {
           window.scrollTo({
@@ -123,14 +66,14 @@ const linkM = 'https://www.saksfifthavenue.com/c/men/apparel';
         try {
           lastPageNumber = document.querySelector('div.col-12.grid-footer > nav > div > ul > li:nth-child(6) > a').innerText
         } catch (e) {
-          console.log(e)
+          logger.info(e)
         }
         return lastPageNumber
       })
 
       // parsing data for every page
 
-      while (counter !== 2) {
+      while (counter !== lastPage) {
         // function to get pid for every item
 
         const pids = await page.evaluate(async () => {
@@ -145,8 +88,7 @@ const linkM = 'https://www.saksfifthavenue.com/c/men/apparel';
               page_1.push(obj)
             })
           } catch (e) {
-            //logger.error(e)
-            console.log(e)
+             logger.info(e)
           }
           return page_1
         })
@@ -172,6 +114,8 @@ const linkM = 'https://www.saksfifthavenue.com/c/men/apparel';
             return data
           }, linkProd)
 
+          console.log(doc.product.attributes[4].attributes)
+
           const obj = {
             uuid: doc.product.uuid !== null
               ? doc.product.uuid
@@ -185,13 +129,16 @@ const linkM = 'https://www.saksfifthavenue.com/c/men/apparel';
             brand: doc.product.brand.name !== null
               ? doc.product.brand.name
               : 'No brand',
-            material: parseLongDesc(doc.product.longDescription),
+            material: doc.product.attributes[4].attributes !== undefined
+              ? doc.product.attributes[4].attributes[3]
+              : 'No material',
             topCategory: doc.product.attributes[4].attributes[0] !== undefined
               ? doc.product.attributes[4].attributes[0].value[0]
               : 'No TopCategory',
-            subCategory: doc.product.attributes[4].attributes[8] !== undefined
-              ? doc.product.attributes[4].attributes[8].value[0]
+            subCategory: doc.product.attributes[4].attributes[doc.product.attributes[4].attributes.length - 1] !== undefined
+              ? doc.product.attributes[4].attributes[doc.product.attributes[4].attributes.length - 1]
               : 'No SubCategory'
+
           }
 
           await res.push(obj)
@@ -201,7 +148,6 @@ const linkM = 'https://www.saksfifthavenue.com/c/men/apparel';
 
         await page.click('p.page-item.d-flex.next')
         await page.waitForSelector('#maincontent > div.container.search-results.hide-designer-on-cat > div > div > div.row.search-result-wrapper.tile-descriptions > div.product-tile-section.col-sm-12.col-md-9 > div.row.product-grid > div:nth-child(27)')
-        //await page.waitForTimeout(4000)
         counter++
         logger.info('Page: ' + (counter + 1))
         logger.info('Scrolling page')
@@ -224,9 +170,9 @@ const linkM = 'https://www.saksfifthavenue.com/c/men/apparel';
         logger.info('Data saved')
       })
     } catch (e) {
-      console.log(e)
+      logger.info(e)
     }
   }
   await parse(linkW)
- // await parse(linkM)
+  await parse(linkM)
 })()
